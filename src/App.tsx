2fd102ab230378aba8f71fbeee6bd16eb6566e87
lucaslopes3d <3d.lucaslopes@gmail.com>
2024-05-24 19:06:21 -0300

    Atualizando arquivos - 24.05

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import "./Styles/global.css"
 import { Header } from './components/Header/Header';
 import { Tasks } from './components/Tasks/Tasks';
 
-function App() {
-  useEffect(() => {
-    console.log("Executando a função de useEffect...")
-  }, [])
 
-  
+function App() {
+useEffect(() => {}, )
+
   return (
     <>
       <Header />
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      'azulheader': '#181c23',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
